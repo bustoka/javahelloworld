8a6913a6456c70610b3dbbe18e88c3e131f85f3e
vagrant <vagrant@ubuntu-14.04-amd64-vbox>
2017-02-24 20:28:23 +0000

    Eight commit

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -9,6 +9,7 @@ public class JavaHelloWorld
 	   System.out.println("hello fifth time for WebhookServer and Jenkins");
            System.out.println("hello sixth time for WebhookServer and Jenkins");
            System.out.println("hello seveth time for branch PullRequest");
+           System.out.println("hello eight time");
 	}
 }
 
